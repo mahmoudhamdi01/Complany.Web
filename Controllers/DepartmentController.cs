43eca994c32c211ada2fd9adf4b989a9e0ff4632
Mahmoud Hamdi <ma7moudselim01@gmail.com>
2024-09-02 18:13:36 +0300

    Company.Web

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,20 @@
+﻿using Company.repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Complany.Web.Controllers
+{
+	public class DepartmentController : Controller
+	{
+		private readonly IDepartmentRepository departmentRepository;
+
+		public DepartmentController(IDepartmentRepository _departmentRepository) 
+		{
+			departmentRepository = _departmentRepository;
+		}
+		public IActionResult Index()
+		{
+			var department = departmentRepository.All();
+			return View(department);
+		}
+	}
+}
